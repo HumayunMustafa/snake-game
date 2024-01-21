d8f5c60b4e95a809fd518847c19eedc26236a55a
HumayunMustafa <khumay20@gmail.com>
2024-01-21 22:00:32 +0500

    working on frames

---
+++ frame.h
@@ -0,0 +1,22 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+class frame
+{
+private:
+    /* data */
+public:
+    frame(/* args */);
+    ~frame();
+};
+
+frame::frame(/* args */)
+{
+}
+
+frame::~frame()
+{
+}
+
+
+#endif
