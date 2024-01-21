652a55a0679b78af1a9fd6ab050bbe4a1c73d169
HumayunMustafa <khumay20@gmail.com>
2024-01-21 22:55:16 +0500

    working on snake logic

---
+++ snake.h
@@ -0,0 +1,16 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+class snake{
+    private:
+        int lenR {};
+        int lenC {};
+        const char head='O';
+        const char body='o';
+        int size=1;
+    public:
+        void checkSize(){
+            
+        }
+};
+
+#endif
